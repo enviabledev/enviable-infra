e3ade99a05055f11e5e6138d01a60da5cb865ad9
Kalu Chinecherem <kaluchinecherem8@gmail.com>
2026-06-18 12:39:08 +0200

    feat(compute): t3.micro ec2 with swap, docker, eip

---
+++ modules/compute/main.tf
@@ -0,0 +1,27 @@
+data "aws_ssm_parameter" "al2023" {
+  name = "/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
+}
+
+resource "aws_instance" "this" {
+  ami                    = data.aws_ssm_parameter.al2023.value
+  instance_type          = "t3.micro"
+  subnet_id              = var.public_subnet_id
+  vpc_security_group_ids = [var.ec2_security_group_id]
+  iam_instance_profile   = var.instance_profile_name
+
+  user_data = templatefile("${path.module}/userdata.sh.tftpl", {})
+
+  root_block_device {
+    volume_type = "gp3"
+    volume_size = 20
+    encrypted   = true
+  }
+
+  tags = { Name = "${var.project}-backend" }
+}
+
+resource "aws_eip" "this" {
+  domain   = "vpc"
+  instance = aws_instance.this.id
+  tags     = { Name = "${var.project}-eip" }
+}
---
+++ modules/compute/variables.tf
@@ -0,0 +1,4 @@
+variable "project" { type = string }
+variable "public_subnet_id" { type = string }
+variable "ec2_security_group_id" { type = string }
+variable "instance_profile_name" { type = string }
