448f60c2864a521eff2b5c2b19d3b711795f0cf5
Kalu Chinecherem <kaluchinecherem8@gmail.com>
2026-06-18 12:23:54 +0200

    feat: terraform remote-state bootstrap (s3 + dynamodb lock)

---
+++ bootstrap/outputs.tf
@@ -0,0 +1,2 @@
+output "state_bucket" { value = aws_s3_bucket.tfstate.bucket }
+output "lock_table" { value = aws_dynamodb_table.tf_lock.name }
---
+++ bootstrap/variables.tf
@@ -0,0 +1,9 @@
+variable "region" {
+  type    = string
+  default = "eu-north-1"
+}
+
+variable "project" {
+  type    = string
+  default = "enviable"
+}
